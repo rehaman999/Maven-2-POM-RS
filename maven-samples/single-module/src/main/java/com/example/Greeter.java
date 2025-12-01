e48cce1bbdbf1d67d93091d3578a72240e82b60f
rehaman999 <144353021+rehaman999@users.noreply.github.com>
2025-12-01 19:28:14 +0530

    Update Greeter.java

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method & Hello team good morning welcome to Session !!!");
+	  System.out.println("Hello from Main Method & Hello team good afternoon welcome to Session !!!");
   }
 	
 }
